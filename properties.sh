d850dce0afc1e740f3d23de772c42d9d6e54d43c
subbussmeo <subbudevops8986@gmail.com>
2021-11-29 11:49:17 +0530

    selenium

---
+++ properties.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+instanceFile=${WORKSPACE}/src/test/resources/application.properties
+if [ -f "$instanceFile" ]; then
+   environment_url=$(cat "$instanceFile" | grep environment.url)
+   application_url=$(cat "$instanceFile" | grep application.url)
+   es_path=$(cat "$instanceFile" | grep es.path)
+   test_planId=$(cat "$instanceFile" | grep test.planId)
+   test_planName=$(cat "$instanceFile" | grep test.planName)
+   file_Content=$(cat "$instanceFile" | grep file.Content)
+
+
+   
+   updated_environment_url="environment.url=$environmenturl"
+   updated_application_url="application.url=$applicationurl"
+   updated_es_path="es.path=$espath"
+   updated_test_planId="test.planId=$testplanId"
+   updated_test_planName="test.planName=$testplanName"
+   updated_file_Content="file.Content=$fileContent"
+    
+   # SED command to replace exisitng line with newly updated line in InstanceFile
+   sed -i "s|$environment_url|$updated_environment_url|g" "$instanceFile"
+   sed -i "s|$application_url|$updated_application_url|g" "$instanceFile"
+   sed -i "s|$es_path|$updated_es_path|g" "$instanceFile"
+   sed -i "s/$test_planId/$updated_test_planId/g" "$instanceFile"
+   sed -i "s/$test_planName/$updated_test_planName/g" "$instanceFile"
+   sed -i "s|$file_Content|$updated_file_Content|g" "$instanceFile"
+fi
